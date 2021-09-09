362ab0cdb5d1fdbccd943bd3fef96ccc9bdf25a8
gurzixo <gurzixo@platinn.com>
2021-09-09 13:55:23 +0100

    script setup in Home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
+<script setup>
+"use strict";
+
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
 <template>
     <div class="home">
         <HelloWorld msg="Welcome to Formio+Vuejs3+Vite App" />
     </div>
 </template>
-
-<script lang="ts">
-"use strict";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default {
-    components: {
-        HelloWorld,
-    },
-};
-</script>
